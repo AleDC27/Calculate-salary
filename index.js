01e8527b50522ad8f8ad17e3584999a972b55de0
AleDC27 <walteralejandrogordillo@gmail.com>
2023-05-01 19:29:00 -0300

    firs commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let hoursField=document.getElementById('hours');
+let moneyField = document.getElementById("money");
+let formField = document.getElementById("form");
+
+
+/* hoursField.addEventListener("change",()=>{
+    let hours = hoursField.value;
+    console.log("Money per Hour:", hours);
+})
+
+moneyField.addEventListener("change", function() {
+    let money = moneyField.value;
+    console.log("Money per Hour:", money);
+  });
+ */
+formField.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let sum=+moneyField.value*(+hoursField.value);
+    document.getElementById("total").innerHTML=sum
+})
+
