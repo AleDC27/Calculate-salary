06aab2c5f971fc224ebde9239b5ea64c905d7880
AleDC27 <walteralejandrogordillo@gmail.com>
2023-06-17 18:02:58 -0300

    sss

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ moneyField.addEventListener("change", function() {
 formField.addEventListener("submit",(e)=>{
     e.preventDefault();
     let sum=+moneyField.value*(+hoursField.value);
-    document.getElementById("total").innerHTML=sum+" te extraño amiguis"
+    document.getElementById("total").innerHTML="El dinero va y viene quiereme papi"
 })
 
